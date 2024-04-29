fcbae1245b6bad0fef3af20f01b1e886f29d0902
Bavario <35093969+bavario-lginc@users.noreply.github.com>
2024-04-29 07:52:12 +0200

    Delete unrelated comment

--- source/main.cpp
+++ source/main.cpp
@@ -4,5 +4,3 @@ void playScenarioSelectAST (const char *, bool) {
     MR::startSubBGM("MBGM_SMG2_GALAXY_08", false);
 }
 kmCall(0x80453154, playScenarioSelectAST);
-
-// 0x8046D1A0 HUGE FIND! (I think)
